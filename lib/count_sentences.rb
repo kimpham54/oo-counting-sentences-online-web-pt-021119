fa98bd6419eb4b8d51502b842420afe4721b6f52
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-03 03:00:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,14 +11,34 @@ class String
   end
 
   def question?
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
 
   end
 
   def count_sentences
-
+    counter = 0
+    if self.end_with?("!")
+      counter += 1
+    elsif self.end_with?("?")
+      counter +=1
+    elsif self.end_with?(".")
+      counter +=1
+    else
+      counter
+    end
   end
+
 end
